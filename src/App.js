ebd1aa4d5a473ef8f36aa80fe23e0b70b400b20e
Krutik Aghera <agherakrutik99@gmail.com>
2023-01-21 23:23:06 +0530

    started drum_machine

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+function App(){
+  const [volume , setVolume] = React.useState(1);
+  const [recording , setRecording] = React.useState("");
+  const [speed, setSpeed] = React.useState(0.5);
+
+  const playRecording = () => {
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-info min-vh-100 text-white">
+      <div className="text-center">
+        <h2>Drum Machine</h2>
+        {
+          
+        }
+        <br />
+        <h4>Volume</h4>
+        <input 
+          type={"range"}
+          step ="0.01"
+          onChange={(e) => setVolume(e.target.value)}
+          value={volume}
+          max="1"
+          min="0"
+          className="w-50"
+          />
+          <h3>{recording}</h3>
+          {
+            recording &&
+            <>
+              <button onClick={playRecording} className="btn btn-success">Play</button>
+              <button onClick={() => setRecording("")} className="btn btn-danger"></button>
+              <br />
+              <h4>Speed</h4>
+              <input 
+                type="range"
+                step={0.01}
+                onChange={(e) => setSpeed(e.target.value)}
+                value={speed}
+                max="1.2"
+                min={0.1}
+                className="w-50"
+              />
+            </>
+          }
+      </div>
     </div>
-  );
+  )
+}
+
+function Pad({clip , volume , setRecording}) {
+  const [active , setActive] = React.useState(false);
+
+  
 }
 
 export default App;
